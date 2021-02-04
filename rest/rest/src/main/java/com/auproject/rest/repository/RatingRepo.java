823d4cbe036c02a4f295835bfb7a42137986b087
Niket Jain <niket.jain@accolitedigital.com>
2021-02-04 23:29:50 +0530

    delete all

--- rest/rest/src/main/java/com/auproject/rest/repository/RatingRepo.java
+++
@@ -1,7 +0,0 @@
-package com.auproject.rest.repository;
-
-import com.auproject.rest.dao.Rating;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RatingRepo extends JpaRepository<Rating, Integer> {
-}
